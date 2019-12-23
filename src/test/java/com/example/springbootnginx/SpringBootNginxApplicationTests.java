dcaf391e7b2553e13bd5e871fb32990aa6e453cf
Shikho Roy <shikhor@eatlbd.com>
2019-12-23 18:07:19 +0600

    Init

---
+++ src/test/java/com/example/springbootnginx/SpringBootNginxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootnginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNginxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
